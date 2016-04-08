9317d16f16da198f6db419cf765b696f7ef95a61
Macgyver01 <Macgyver01@users.noreply.github.com>
2016-04-08 08:19:48 -0400

    Registration.class

--- app/src/main/java/net/nofool/dev/tbd/Login.java
+++ app/src/main/java/net/nofool/dev/tbd/Login.java
@@ -1,5 +1,6 @@
 package net.nofool.dev.tbd;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +14,7 @@ public class Login extends AppCompatActivity {
     private Button loginB;
     private EditText emailET;
     private EditText passwordET;
+    private Button newB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +24,10 @@ public class Login extends AppCompatActivity {
         loginB = (Button)findViewById(R.id.loginButton);
         emailET = (EditText)findViewById(R.id.emailEditText);
         passwordET = (EditText)findViewById(R.id.passwordEditText);
+        newB = (Button)findViewById(R.id.registerButton);
 
         loginB.setOnClickListener(loginClick);
-
+        newB.setOnClickListener(newClick);
 
     }
     private View.OnClickListener loginClick = new View.OnClickListener() {
@@ -35,6 +38,13 @@ public class Login extends AppCompatActivity {
             email = emailET.getText().toString();
             password = passwordET.getText().toString();
 
+        }
+    };
+    private View.OnClickListener newClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent i = new Intent(getApplicationContext(),Registration.class);
+            startActivity(i);
         }
     };
 
--- app/src/main/java/net/nofool/dev/tbd/Registration.java
+++ app/src/main/java/net/nofool/dev/tbd/Registration.java
@@ -1,8 +1,6 @@
 package net.nofool.dev.tbd;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,6 +14,16 @@ import android.widget.EditText;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class Registration extends AppCompatActivity {
 
     private Button signupButton;
@@ -23,9 +31,11 @@ public class Registration extends AppCompatActivity {
     private EditText passwordField;
     private EditText passwordCheckField;
 
-    private ProgressDialog prog;
+    private String TAG = Registration.class.getSimpleName();
+    private JSONObject jo;
 
-
+    private String pass;
+    private String passCheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +47,9 @@ public class Registration extends AppCompatActivity {
         passwordField = (EditText)findViewById(R.id.passwordEditText);
         passwordCheckField = (EditText)findViewById(R.id.passwordCheckEditText);
 
+        pass = passwordField.getText().toString();
+        passCheck = passwordCheckField.getText().toString();
+
         signupButton.setClickable(false);
 
         passwordCheckField.addTextChangedListener(new TextWatcher() {
@@ -45,7 +58,7 @@ public class Registration extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (passCheck()){
+                if (passCheck(pass, passCheck)){
                     passwordCheckField.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.colorGood));
                     signupButton.setClickable(true);
                 }else {
@@ -66,55 +79,91 @@ public class Registration extends AppCompatActivity {
         private String p;
         @Override
         public void onClick(View v) {
-            e = emailField.toString();
-            p = passwordField.toString();
-            if(register(e, p)){
-                Intent i = new Intent(getApplicationContext(), Login.class);
-                startActivity(i);
-            } else {
-                alertUser("duplicate");
-            }
-
+            e = emailField.getText().toString();
+            p = passwordField.getText().toString();
+            Register2(e, p);
         }
     };
-    private boolean passCheck(){
-        if (passwordField.equals(passwordCheckField)){
+    private boolean passCheck(String p1, String p2){
+        if (p1.equals(p2)){
             return true;
         }else{
             return false;
         }
     }
-    private Boolean register(String email, String pass){
+    private void Register(String email, String pass) {
+        final String e = email;
+        final String p = pass;
+        final String urlRequest = "http://dev.nofool.net/db_getpassword.php";
+        final RequestBody bodyRequest = new FormBody.Builder()
+                .add("email", email)
+                .build();
+        final OkHttpClient client = new OkHttpClient();
+        final Request request = new Request.Builder().url(urlRequest).get().post(bodyRequest).build();
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+            }
 
-
-        return false;
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    final String jsonData = response.body().string();
+                    Log.v(TAG, jsonData);
+                    if (response.isSuccessful()) {
+                        JSONObject jsonObject = new JSONObject(jsonData);
+                        int x =jsonObject.getInt("success");
+                        if (x==1){
+                            alertUser("duplicate");
+                        }
+                        else {
+                            Log.v(TAG,"Register1.onResoponse,else: "+x+" "+e+" "+ p);
+                            Register2(e, p);
+                        }
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "Exception", e);
+                } catch (JSONException e) {
+                    Log.e(TAG, "JSON Exception", e);
+                }
+            }
+        });
     }
+    private void Register2(String e, String p){
+        final String urlPost = "http://dev.nofool.net/db_registar.php";
+        final RequestBody bodyPost = new FormBody.Builder().add("email", e).add("password", p).build();
+        final OkHttpClient client2 = new OkHttpClient();
+        Request request2 = new Request.Builder().url(urlPost).post(bodyPost).build();
+        Call call2 = client2.newCall(request2);
+        call2.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {Log.v(TAG,"Register2.onFailure");}
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    final String jsonData2 = response.body().string();
+                    Log.v(TAG, jsonData2);
+                    if (response.isSuccessful()){
+                        JSONObject jsonO = new JSONObject(jsonData2);
+                        int success = jsonO.getInt("success");
+                        if (success == 1) {
+                            Intent i = new Intent(getApplicationContext(), Login.class);
+                            startActivity(i);
+                        }
+                    }
+                }catch (IOException e){
+                    Log.e(TAG,"Exception",e);
+                }catch (JSONException e){
+                    Log.e(TAG, "JSON Exception", e);
+                }
+            }
+        });
+    }
+
 
     private void alertUser(String s){
         NotificationsDialogFragment alert = new NotificationsDialogFragment();
         alert.show(getFragmentManager(), s);
-    }
-
-
-    class GetUser extends AsyncTask<String, String, String>{
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            prog = new ProgressDialog(Registration.this);
-            prog.setMessage("Creating User..");
-            prog.setIndeterminate(false);
-            prog.setCancelable(false);
-            prog.show();
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-        }
     }
 }
