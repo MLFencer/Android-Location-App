a440a0b47f34985f616e1a73b2aa7e5cd9286d2a
Macgyver01 <Macgyver01@users.noreply.github.com>
2016-04-19 22:55:22 -0400

    Network Check: Works

--- app/src/main/java/net/nofool/dev/tbd/LaunchBoard.java
+++ app/src/main/java/net/nofool/dev/tbd/LaunchBoard.java
@@ -1,6 +1,11 @@
 package net.nofool.dev.tbd;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,9 +17,38 @@ public class LaunchBoard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch_board);
 
+        if (isNetworkAvailible()){
+            Intent i = new Intent(LaunchBoard.this,Login.class);
+            startActivity(i);
+        }else{
+            networkStateResponce();
+        }
 
-        Intent i = new Intent(LaunchBoard.this,Login.class);
-        startActivity(i);
+
     }
 
+    private boolean isNetworkAvailible(){
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo active = connectivityManager.getActiveNetworkInfo();
+        return active != null && active.isConnected();
+    }
+
+    private void networkStateResponce(){
+        AlertDialog.Builder b=new AlertDialog.Builder(LaunchBoard.this)
+                .setTitle("Warning")
+                .setMessage("This Application Requires an Internet Connection!")
+                .setNeutralButton("Retry", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if(isNetworkAvailible()){
+                            Intent i = new Intent(LaunchBoard.this,Login.class);
+                            startActivity(i);
+                        }else{
+                            networkStateResponce();
+                        }
+                    }
+                });
+        AlertDialog d = b.create();
+        d.show();
+    };
 }
